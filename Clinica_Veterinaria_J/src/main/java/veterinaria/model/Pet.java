e86522b718b81b1483893e22cd3cecf0fd04bd3a
LZGabriel1409 <lg.moraes.lima@gmail.com>
2026-08-29 23:39:11 -0300

    Add files via upload

--- Clinica_Veterinaria_J/src/main/java/veterinaria/model/Pet.java
+++ Clinica_Veterinaria_J/src/main/java/veterinaria/model/Pet.java
@@ -59,4 +59,9 @@ public class Pet {
 
     public double getPeso() { return peso; }
     public void setPeso(double peso) { this.peso = peso; }
+
+    @Override
+    public String toString() {
+        return this.nome + " (" + this.especie + ")";
+    }
 }
--- Clinica_Veterinaria_J/src/main/java/veterinaria/model/Tutor.java
+++ Clinica_Veterinaria_J/src/main/java/veterinaria/model/Tutor.java
@@ -45,4 +45,9 @@ public class Tutor {
 
     public String getEndereco() {return endereco;}
     public void setEndereco(String endereco) {this.endereco = endereco;}
+
+    @Override
+    public String toString() {
+        return this.nome + " (CPF: " + this.cpf + ")";
+    }
 }
